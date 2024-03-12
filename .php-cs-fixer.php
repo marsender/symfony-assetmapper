77c3cc3f680a9a990fbb639cd41795244499f1ea
marsender <didier.corbiere@opale-concept.com>
2024-03-12 21:13:07 +0100

    Add cs-fixer #3

---
+++ .php-cs-fixer.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+	->in(__DIR__)
+	->exclude('var')
+	->exclude('vendor');
+
+$config = new PhpCsFixer\Config();
+
+// https://cs.symfony.com/doc/rules/index.html
+return $config->setRules([
+	'@Symfony' => true,
+	'@PSR12' => true,
+	'ordered_imports' => true,
+])->setIndent("\t")
+	->setLineEnding("\n")
+	->setFinder($finder);
